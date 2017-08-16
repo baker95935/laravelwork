20eb888c4f758df76ceb80caa02da2ccb96fa24b
baker95935 <baker95935@qq.com>
2017-08-16 20:58:21 +0800

    merge

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,11 +9,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix' => 'admin','namespace' => 'Admin'],function ($router)
+{
+	$router->get('login', 'LoginController@showLoginForm')->name('admin.login');
+	$router->post('login', 'LoginController@login');
+	$router->post('logout', 'LoginController@logout');
+	$router->get('dash', 'DashboardController@index');
+});
+ 
